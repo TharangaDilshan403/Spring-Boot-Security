598d783737cdf4ee4c5c1c438f209501232b64c1
Tharanga_Dilshan <59679693+TharangaDilshan403@users.noreply.github.com>
2021-12-24 22:27:08 +0530

    JWT Token
    JWT TOKEN generated successfully and log-in function working.

--- src/main/java/com/practical/test/jwt/controller/HelloController.java
+++ src/main/java/com/practical/test/jwt/controller/HelloController.java
@@ -1,15 +1,40 @@
 package com.practical.test.jwt.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.practical.test.jwt.dto.UserDTO;
+import com.practical.test.jwt.utility.JWTUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
 public class HelloController {
+
+    @Autowired
+    private JWTUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
 
     @GetMapping
     public String save(){
         return "Success..";
     }
+
+
+//    jwt
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody UserDTO userDTO) throws  Exception{
+
+        try{
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userDTO.getUserName(),userDTO.getUserPassword())
+        );
+    }catch (Exception ex){
+            throw new Exception("Invalid user name and password...");
+        }
+        return jwtUtility.generateToken(userDTO.getUserName());
+    }
+
 }
---
+++ src/main/java/com/practical/test/jwt/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.practical.test.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String userName;
+    private String userPassword;
+}
