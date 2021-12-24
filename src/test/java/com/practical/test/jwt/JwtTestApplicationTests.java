dbbe1432b32fc47a83c213516951d501e13f9b1b
Tharanga_Dilshan <59679693+TharangaDilshan403@users.noreply.github.com>
2021-12-24 15:49:07 +0530

    first commit

---
+++ src/test/java/com/practical/test/jwt/JwtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practical.test.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
